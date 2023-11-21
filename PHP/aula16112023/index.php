bba660cdbc6001c0590ee3d07713d57ee0b385f9
Tauani Vitória <tauanivitoria1302@gmail.com>
2023-11-20 22:57:18 -0300

    Bootstrap nas telas

--- PHP/aula16112023/index.php
+++ PHP/aula16112023/index.php
@@ -10,14 +10,20 @@
     <html lang="pt-br">
         <?php require 'head.php';?>
     <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Tabela Pessoa - conexão com Banco</title>
         <style title="text/css">
+            body{
+                background-color: #e2be8b;
+            }
             .titulo_tabela h1{
                 display: flex;
                 justify-content: center;
                 align-items: center; 
+                margin-top: 20px;
+                margin-bottom: 10px;
             }
             .botao{
                 margin-bottom: 50px;
@@ -43,8 +49,10 @@
         
     </head>
     <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <div class="titulo_tabela">
             <h1>Tabela Pessoa </h1>
+            <br>
         </div>
 
     <!--     <div class="botao">
@@ -56,18 +64,18 @@
             <a >Excluir</a>
         </div> -->
         <div class="tabela">
-            <table>
-                <thread>
+            <table class="table table-hover table-secondary">
+                <thead  class="table-dark">
                 <tr>
-                    <th>Nome</th>
-                    <th>Idade</th>
-                    <th>Sexo</th>
-                    <th>Endereço</th>
-                    <th>Edição</th>
-                    <th>Exclusão</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Idade</th>
+                    <th scope="col">Sexo</th>
+                    <th scope="col">Endereço</th>
+                    <th scope="col">Edição</th>
+                    <th scope="col">Exclusão</th>
                 </tr>
-                </thread>
-                <tbody>
+                </thead>
+                <tbody class="table-group-divider">
                     <?php foreach($dados as $item): ?>
                         <tr>
                         <td><?php echo $item['nome']; ?> </td>
